d90bccb0a5b46441e9ad0d9bddf1da852b6700f5
Nouha Ben Zayed <Benzayednouha54@gmail.com>
2024-03-03 13:12:05 +0100

    W2 My first React component

---
+++ hacker-stories/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function getTitle(title) {
+  return title;
+}
+function App() {
+  return (
+    <div>
+      <h1>Hello {getTitle("React")}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App;
